97af87d8e01e14da774f8fc20b70ec0bbfba7d57
daniellx42 <daniellx42.job@gmail.com>
2022-05-12 18:45:16 -0300

    version 2.0 changes on layout and components added

---
+++ db_skills.js
@@ -0,0 +1,154 @@
+const db_skills = [
+  {
+    id: 1,
+    name: "rust",
+    text: "Learning language.",
+    image: "./images/rust.svg",
+  },
+  {
+    id: 2,
+    name: "JavaScript",
+    text: "1 year and 6 month of experience.",
+    image: "./images/javascript.svg",
+  },
+  {
+    id: 3,
+    name: "TypeScript",
+    text: "1 year 6 month of experience.",
+    image: "./images/typescript.svg",
+  },
+  {
+    id: 4,
+    name: "Nodejs",
+    text: "1 year and 6 month of experience.",
+    image: "./images/nodejs.svg",
+  },
+  {
+    id: 5,
+    name: "Reactjs",
+    text: "1 year of experience.",
+    image: "./images/react.svg",
+  },
+  {
+    id: 6,
+    name: "HTML",
+    text: "1 year and 6 month of experience.",
+    image: "./images/html5.svg",
+  },
+  {
+    id: 7,
+    name: "CSS3",
+    text: "1 year and 6 month of experience.",
+    image: "./images/css3.svg",
+  },
+  {
+    id: 8,
+    name: "Mongodb",
+    text: "1 year of experience.",
+    image: "./images/mongodb.svg",
+  },
+  {
+    id: 9,
+    name: "Docker",
+    text: "1 year and 6 month of experience.",
+    image: "./images/docker.svg",
+  },
+  {
+    id: 10,
+    name: "AWS",
+    text: "5 month of experience.",
+    image: "./images/amazonaws.svg",
+  },
+  {
+    id: 11,
+    name: "heroku",
+    text: "4 monyh of experience.",
+    image: "./images/heroku.svg",
+  },
+  {
+    id: 12,
+    name: "ubuntu",
+    text: "2 year of experience.",
+    image: "./images/ubuntu.svg",
+  },
+  {
+    id: 13,
+    name: "archlinux",
+    text: "2 year of experience.",
+    image: "./images/archlinux.svg",
+  },
+  {
+    id: 14,
+    name: "git",
+    text: "1 year and 6 month of experience.",
+    image: "./images/git.svg",
+  },
+  {
+    id: 15,
+    name: "github",
+    text: "1 year and 6 month of experience.",
+    image: "./images/github.svg",
+  },
+  {
+    id: 16,
+    name: "vim",
+    text: "2 year of experience.",
+    image: "./images/vim.svg",
+  },
+  {
+    id: 17,
+    name: "Visual Studio Code Insiders",
+    text: "1 year 6 month of experience.",
+    image: "./images/vscode.svg",
+  },
+  {
+    id: 18,
+    name: "Figma",
+    text: "2 month of experience.",
+    image: "./images/figma.svg",
+  },
+  {
+    id: 19,
+    name: "Bootstrap",
+    text: "4 month of experience.",
+    image: "./images/bootstrap.svg",
+  },
+  {
+    id: 20,
+    name: "Insomnia",
+    text: "6 month experience.",
+    image: "./images/insomnia.svg",
+  },
+  {
+    id: 21,
+    name: "Postman",
+    text: "6 month experience.",
+    image: "./images/postman.svg",
+  },
+  {
+    id: 22,
+    name: "jest",
+    text: "2 month experience.",
+    image: "./images/jest.svg",
+  },
+];
+
+const skills = () => {
+  const class_skills = document.querySelector(".card-container");
+  return db_skills.map((item) => {
+    console.log(item);
+    class_skills.innerHTML += `
+            <div class="card-content">
+              <div class="card">
+              <div class="card-image">
+                  <img src="${item.image}">
+              </div>
+              <div class="card-content">
+                  <h3>${item.name}</h3>
+                  <p>${item.text}</p>
+              </div>
+          </div>
+          </div>
+            `;
+  });
+};
---
+++ index.js
@@ -0,0 +1,57 @@
+const navBar = () => {
+  const nav = document.querySelector(".navbar");
+  fetch("https://api.github.com/users/daniellx42")
+    .then(async (res) => {
+      if (!res.ok) {
+        throw Error(res.status);
+      }
+      let data = await res.json();
+      return (nav.innerHTML += `
+      <div class="navbar">
+        <div class="avatar">
+          <img src="${data.avatar_url}"/>
+        </div>
+        <p>${data.name}</p>
+        <p id="p-bio">${data.bio}</p>
+          <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="projects_github.html">
+                Projects GitHub - ${data.public_repos}</a>
+              </li>
+              <li><a href="skills.html">Skills</a></li>
+          </ul>
+      </div>
+    `);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getApiGithHubRepos = () => {
+  const api_github_Repos = document.querySelector(".api-github");
+  fetch("https://api.github.com/users/daniellx42/repos")
+    .then(async (res) => {
+      if (!res.ok) {
+        throw Error(res.status);
+      }
+      let data = await res.json();
+      return data.map((item) => {
+        api_github_Repos.insertAdjacentHTML(
+          "beforeend",
+          `
+    <div class="card">
+      <a href="${item.html_url}" target="_blank">
+        <div>
+          <p>${item.name}</p>
+        </div>
+      </a>
+    </div>
+  `
+        );
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
